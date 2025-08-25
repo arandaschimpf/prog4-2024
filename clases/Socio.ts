b16cf665d4136d04051050d493a5298a1636933f
Enzo Pitana <enzopitana@gmail.com>
2025-08-25 18:17:40 -0300

    Update Socio.ts

--- clases/Socio.ts
+++ clases/Socio.ts
@@ -1,4 +1,3 @@
-// Archivo: Socio.ts
 import { Libro } from "./Libro";
 import { Prestamo } from "./Prestamo";
 
@@ -23,14 +22,14 @@ export class Socio {
   get deuda(): number { return this._deuda; }
   get historialLectura(): Libro[] { return this._historialLectura; }
 
-  // Cuando se lleva un libro prestado
+  // se lleva un libro prestado
   retirar(libro: Libro, duracion: number): void {
     const vencimiento = new Date();
     vencimiento.setDate(vencimiento.getDate() + duracion);
     this.prestamos.push(new Prestamo(libro, vencimiento));
   }
 
-  // Cuando devuelve un libro
+  // devuelve un libro
   devolver(libro: Libro): void {
     const prestamo = this.prestamos.find((p) => p.libro.isbn === libro.isbn);
     if (!prestamo) {
@@ -42,18 +41,18 @@ export class Socio {
       const diasDeRetraso = Math.floor((hoy.getTime() - prestamo.vencimiento.getTime()) / (1000 * 60 * 60 * 24));
       const multa = diasDeRetraso * 50;
       this._deuda += multa;
-      console.log(`Ups! "${libro.titulo}" se entregó ${diasDeRetraso} días tarde. Eso suma $${multa} a tu cuenta. Tu deuda total ahora es de $${this._deuda}. ¡La próxima tratá de devolverlo a tiempo!`);
+      console.log(`${libro.titulo} se entregó ${diasDeRetraso} días tarde. Esto suma $${multa} a tu cuenta. Tu deuda total ahora es de $${this._deuda}`);
     } else {
-      console.log(`¡Perfecto! Lo devolviste a tiempo. Sin multas ni problemas.`);
+      console.log(`Lo devolviste a tiempo`);
     }
 
     const indice = this.prestamos.indexOf(prestamo);
     this.prestamos.splice(indice, 1);
     this._historialLectura.push(libro);
-    console.log(`"${libro.titulo}" se agregó a tu historial de lectura, ${this.nombreCompleto}. ¡Qué bueno que hayas disfrutado otro libro!`);
+    console.log(`"${libro.titulo}" se agregó a tu historial de lectura, ${this.nombreCompleto}`);
   }
 
-  // Verificamos si tiene prestado un libro específico
+  // Verificacion si tiene prestado un libro especifico
   tienePrestadoLibro(libro: Libro): Prestamo | undefined {
     return this.prestamos.find((p) => p.libro.isbn === libro.isbn);
   }
@@ -61,6 +60,6 @@ export class Socio {
   // Pagar las deudas pendientes
   saldarDeuda(): void {
     this._deuda = 0;
-    console.log(`¡Genial ${this.nombreCompleto}! Tu deuda está saldada. Ya podés llevarte libros de nuevo sin problemas.`);
+    console.log(`${this.nombreCompleto}, Tu deuda está saldada`);
   }
 }
