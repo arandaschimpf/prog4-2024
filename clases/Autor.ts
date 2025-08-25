792e8ccec2ff106f728d85edb55358fc9f9d0850
Enzo Pitana <enzopitana@gmail.com>
2025-08-25 16:03:07 -0300

    Create Autor.ts

---
+++ clases/Autor.ts
@@ -0,0 +1,20 @@
+// En el archivo Autor.ts
+export class Autor {
+  constructor(
+    private _nombre: string,
+    private _biografia: string,
+    private _anoNacimiento: number
+  ) {}
+
+  get nombre(): string {
+    return this._nombre;
+  }
+  
+  get biografia(): string {
+    return this._biografia;
+  }
+  
+  get anoNacimiento(): number {
+    return this._anoNacimiento;
+  }
+}
